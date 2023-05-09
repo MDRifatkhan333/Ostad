db892e0e006cca4cbaeadc316c4250a86f4ef874
MDRifatkhan333 <rakibulhasanrifat2031@gmail.com>
2023-05-09 22:43:24 +0600

    Null Safety

---
+++ bin/practicesCode/module3/null_safty.dart
@@ -0,0 +1,12 @@
+void main() {
+  int? age;
+  String? name;
+  double? salary;
+  // print null value
+  print(age);
+  age = 10;
+  print(name);
+  name = "John";
+  print(name);
+  print(age);
+}
